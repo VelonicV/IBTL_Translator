4631f971f65c96c293681c7c77d462c7dff10ed2
David Bereza <velonicv@gmail.com>
2015-01-24 20:00:39 -0800

    Initial token classes added.

---
+++ CS480Translator/Tokens/IntegerConstantToken.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS480Translator.Tokens
+{
+    class IntegerConstantToken : GenericToken
+    {
+
+        public int integer;
+
+        public IntegerConstantToken(string value)
+        {
+            if (!validate(value))
+            {
+                throw new Exception("Error: Invalid integer string passed into token");
+            }
+        }
+
+        protected override bool validate(string value)
+        {
+            try
+            {
+                integer = Convert.ToInt32(value);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
