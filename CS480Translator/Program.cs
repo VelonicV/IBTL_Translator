c52458158c6fac0b2d54389b236249d1f9fdc474
David Bereza <velonicv@gmail.com>
2015-02-11 23:55:26 -0800

    added Node class and necessary logic, tested

--- CS480Translator/Program.cs
+++ CS480Translator/Program.cs
@@ -11,23 +11,33 @@ namespace CS480Translator
         static void Main(string[] args)
         {
 
-            SymbolTable st = new SymbolTable();
-            Lexalizer lex = new Lexalizer(args[0]);
-            Tokens.GenericToken token = lex.getNextToken();
+            //SymbolTable st = new SymbolTable();
+            //Lexalizer lex = new Lexalizer(args[0]);
+            //Tokens.GenericToken token = lex.getNextToken();
 
-            while (token != null)
-            {
-                if (token is Tokens.IT)
-                {
-                    st.add((Tokens.IT) token);
-                }
+            //while (token != null)
+            //{
+            //    if (token is Tokens.IT)
+            //    {
+            //        st.add((Tokens.IT) token);
+            //    }
 
-                Console.WriteLine(token.ToString());
-                token = lex.getNextToken();
-            }
+            //    Console.WriteLine(token.ToString());
+            //    token = lex.getNextToken();
+            //}
 
-            Console.WriteLine("\nSymbol Table entries: {0}", st.count());
-            //Console.ReadLine();
+            //Console.WriteLine("\nSymbol Table entries: {0}", st.count());
+            ////Console.ReadLine();
+
+            //Node root = new Node(null, null);
+            //Node rootChild1 = new Node(root, new Tokens.IT("test"));
+            //Node rootChild2 = new Node(root, new Tokens.IT("test2"));
+            //Node rootChild3 = new Node(root, new Tokens.IT("test3"));
+            //Node rootChild2Child1 = new Node(rootChild2, new Tokens.IT("test4"));
+            //Node rootChild2Child2 = new Node(rootChild2, new Tokens.IT("test5"));
+
+            //Node.postOrderTraversal(root);
+            //Console.Read();
 
         }
     }
